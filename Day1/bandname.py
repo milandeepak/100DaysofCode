6904a128afa430268467e1706bc6875d39c6fa8b
Milan Deepak <milandeepak06@gmail.com>
2022-09-11 10:20:44 +0530

    add day1 folder

---
+++ Day1/bandname.py
@@ -0,0 +1,7 @@
+print("Welcome to Band Name Generator.")
+# created a variable called city and stored value 
+city=input("What's the name of the city you grew up in?")
+# created a variable called pet and stored value 
+pet =input("What's your pet's name?")
+#using f strings , printed the variables, (city+pet, .format(city,pet are alternatives))
+print(f"Your band name could be {city} {pet}")
